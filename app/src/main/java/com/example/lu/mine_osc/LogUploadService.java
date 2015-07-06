169d88d7f9db37b2a7fc3fed59026096c4d4c910
jones-junior <chalengemyself@gmail.com>
2015-07-07 00:23:47 +0800

    修改错误

--- app/src/main/java/com/example/lu/mine_osc/LogUploadService.java
+++ app/src/main/java/com/example/lu/mine_osc/LogUploadService.java
@@ -4,8 +4,11 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 
+import org.kymjs.kjframe.utils.FileUtils;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class LogUploadService extends Service {
     public LogUploadService() {
@@ -19,8 +22,13 @@ public class LogUploadService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-//        File log = FileUtils.
-//        FileInputStream file = new FileInputStream();
+        File log = FileUtils.getSaveFile("OSChina","OSCLog");
+        try {
+            FileInputStream stream = new FileInputStream(log);
+//            String data = stream
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
         return super.onStartCommand(intent, flags, startId);
     }
 }
