cd9a2c258addeda0d8001f76f15dfa826da71d42
wk_company_local <chalengemyself@gail.com>
2015-07-15 15:27:50 +0800

    11

--- app/src/main/java/com/example/lu/mine_osc/base/BaseApplication.java
+++ app/src/main/java/com/example/lu/mine_osc/base/BaseApplication.java
@@ -5,8 +5,12 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.os.Build;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.lu.mine_osc.R;
 import com.example.lu.mine_osc.util.StringUtil;
@@ -104,12 +108,38 @@ public class BaseApplication extends Application{
         return context().getSharedPreferences(PREF_NAME,Context.MODE_MULTI_PROCESS);
     }
 
+
+    /**
+     *自定义土司
+     * @param message
+     * @param duration
+     * @param icon
+     * @param gravity
+     */
     public static void showToast(String message,int duration,int icon,int gravity){
         if(message!=null&&!message.equalsIgnoreCase("")){
             long time = System.currentTimeMillis();
             if(!message.equalsIgnoreCase(lastToast)||Math.abs(time - lastToastTime)>2000){
                 View view = LayoutInflater.from(context()).inflate(R.layout.view_toast,null);
+                ((TextView)view.findViewById(R.id.title_tv)).setText(message);
+                if (icon != 0){
+                    ((ImageView)view.findViewById(R.id.icon_iv)).setImageResource(icon);
+                    ((ImageView)view.findViewById(R.id.icon_iv)).setVisibility(View.VISIBLE);
+                }
+                Toast toast = new Toast(context());
+                toast.setView(view);
+                if (gravity == Gravity.CENTER){
+                    toast.setGravity(gravity,0,0);
+                }else{
+                    toast.setGravity(gravity,0,35);
+                }
+                toast.setDuration(duration);
+                toast.show();
+                lastToast = message;
+                lastToastTime = System.currentTimeMillis();
             }
         }
     }
+
+
 }
